54588edb760cba8e581bc553139777819c0d4128
William Bryce Angell <bryce.angell@resi.io>
2022-12-14 10:08:26 -0600

    add navigation to the hardware view

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,11 @@ type Props = {
 };
 
 const Router: React.FC<Props> = ({ navigate }) => {
-  if (hardwareListRoute.test(window.location.href))
+  if (
+    hardwareListRoute.test(window.location.href) &&
+    (hardwareListRoute.exec(window.location.href) as URLPatternResult).pathname
+      .input === hardwareListRoute.pathname
+  )
     return <ListViewHardware navigate={navigate} />;
   return <div>Not Found</div>;
 };
